9bab61be4a1bbb5560fdda139c8439a5aaaf64e6
André Lucas <andrelucas@Andres-Air.Home>
2023-05-08 07:39:49 +0100

    start project

---
+++ tdd-na-pratica/src/main/java/com/programaremmarte/tdd-na-pratica/App.java
@@ -0,0 +1,13 @@
+package com.programaremmarte.tdd-na-pratica;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
